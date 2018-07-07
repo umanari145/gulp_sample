ae26b2c9f8fe9fdaa833a17cf2468301d726f088
Norio <umanari145@gmail.com>
2018-07-07 22:54:26 +0900

    deleteメソッド追加&package.json追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp')
-
+const del = require('del')
 pathInfo = {
   srcDir:'src',
   distDir: 'htdocs'
 }
 
+
 //srcからdistへの移動
 //ディレクトリの作成も含む
-gulp.task('move', () => {
+gulp.task('move', function() {
+  console.log('--task move--')
   //ファイルの読み込み
   //完全再帰にする場合はディレクトリの深度分下記のようにかく
   //特定拡張子の場合は/*/*.scssなど
@@ -15,4 +17,14 @@ gulp.task('move', () => {
   //パイプは単なる結合
   //destは吐き出し
       .pipe(gulp.dest(pathInfo.distDir + '/'))
+})
+
+/**
+ * 生成ファイルの削除(一番最初に行う)
+ */
+gulp.task('clean', function(){
+  console.log('--task clear--')
+  del([
+    pathInfo.distDir
+  ])
 })
